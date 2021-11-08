362f607cdc56e85e3ef61d291cdc32c11df725de
ParijatDhalkar <parijatddhalkar@gmail.com>
2021-11-08 20:26:36 +0530

    Fertilizer recommendation

--- app/app.py
+++ app/app.py
@@ -71,6 +71,11 @@ crop_recommendation_model_path = 'models/RandomForest.pkl'
 crop_recommendation_model = pickle.load(
     open(crop_recommendation_model_path, 'rb'))
 
+# Loading fertilizer recommendation model
+
+fert_recommendation_model_path = 'models/FertilizerRecommender.pkl'
+fert_recommendation_model = pickle.load(
+    open(fert_recommendation_model_path, 'rb'))
 
 # =========================================================================================
 
@@ -141,17 +146,17 @@ def home():
 
 @ app.route('/crop-recommend')
 def crop_recommend():
-    title = 'Harvestify - Crop Recommendation'
+    title = 'Smart Farming - Crop Recommendation'
     return render_template('crop.html', title=title)
 
 # render fertilizer recommendation form page
 
 
-# @ app.route('/fertilizer')
-# def fertilizer_recommendation():
-#     title = 'Harvestify - Fertilizer Suggestion'
+@ app.route('/fertilizer')
+def fertilizer_recommendation():
+    title = 'Smart Farming - Fertilizer Suggestion'
 
-#     return render_template('fertilizer.html', title=title)
+    return render_template('fertilizer.html', title=title)
 
 #render disease prediction input page
 
@@ -194,46 +199,25 @@ def crop_prediction():
 # render fertilizer recommendation result page
 
 
-# @ app.route('/fertilizer-predict', methods=['POST'])
-# def fert_recommend():
-#     title = 'Harvestify - Fertilizer Suggestion'
+@ app.route('/fertilizer-predict', methods=['POST'])
+def fert_recommend():
+    title = 'Harvestify - Fertilizer Recommendation'
 
-#     crop_name = str(request.form['cropname'])
-#     N = int(request.form['nitrogen'])
-#     P = int(request.form['phosphorous'])
-#     K = int(request.form['pottasium'])
-#     # ph = float(request.form['ph'])
+    if request.method == 'POST':
+        N = int(request.form['nitrogen'])
+        P = int(request.form['phosphorous'])
+        K = int(request.form['pottasium'])
+        ph = float(request.form['ph'])
+        soil_moisture = int(request.form['soil_moisture'])
 
-#     df = pd.read_csv('Data/fertilizer.csv')
+        data = np.array([[N, P, K, ph, soil_moisture]])
+        my_prediction = fert_recommendation_model.predict(data)
+        final_prediction = my_prediction[0]
 
-#     nr = df[df['Crop'] == crop_name]['N'].iloc[0]
-#     pr = df[df['Crop'] == crop_name]['P'].iloc[0]
-#     kr = df[df['Crop'] == crop_name]['K'].iloc[0]
+        response = Markup(str(fertilizer_dic[final_prediction]))
+        
+        return render_template('fertilizer-result.html', recommendation=response,title=title)
 
-#     n = nr - N
-#     p = pr - P
-#     k = kr - K
-#     temp = {abs(n): "N", abs(p): "P", abs(k): "K"}
-#     max_value = temp[max(temp.keys())]
-#     if max_value == "N":
-#         if n < 0:
-#             key = 'NHigh'
-#         else:
-#             key = "Nlow"
-#     elif max_value == "P":
-#         if p < 0:
-#             key = 'PHigh'
-#         else:
-#             key = "Plow"
-#     else:
-#         if k < 0:
-#             key = 'KHigh'
-#         else:
-#             key = "Klow"
-
-#     response = Markup(str(fertilizer_dic[key]))
-
-#     return render_template('fertilizer-result.html', recommendation=response, title=title)
 
 # render disease prediction result page
 
